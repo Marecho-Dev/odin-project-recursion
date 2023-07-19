b9dbbbd4985d3405b08803c32e4367ce824167ca
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-07-18 21:57:37 -0400

    fib sequence using iteration

---
+++ recursion.js
@@ -0,0 +1,15 @@
+//Assignment 1
+//Fibonacci Sequence using iteration
+function fibs(x) {
+  if (x === 0) return [0];
+  if (x === 1) return [1];
+  fibArr = [0, 1];
+  for (let i = 2; i < x; i++) {
+    fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
+  }
+  return fibArr;
+}
+
+console.log(fibs(8));
+
+//Fibonacci Sequence using recursion
