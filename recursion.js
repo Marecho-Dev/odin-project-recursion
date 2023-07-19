e28f41dbe92b3c280ac1497bed28ce8698e3a504
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-07-18 21:58:06 -0400

    fixed iteration fib sequence

--- recursion.js
+++ recursion.js
@@ -2,7 +2,7 @@
 //Fibonacci Sequence using iteration
 function fibs(x) {
   if (x === 0) return [0];
-  if (x === 1) return [1];
+  if (x === 1) return [0, 1];
   fibArr = [0, 1];
   for (let i = 2; i < x; i++) {
     fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
